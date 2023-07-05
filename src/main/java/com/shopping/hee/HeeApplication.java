6871c03ad7c0b41b4b90a6d2e2c5951ac7ec7093
shin_yehee <zia7894@naver.com>
2023-07-05 21:20:53 +0900

    Initial commit

---
+++ src/main/java/com/shopping/hee/HeeApplication.java
@@ -0,0 +1,13 @@
+package com.shopping.hee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeeApplication.class, args);
+	}
+
+}
