fdf4033d418b0f4524029940974426b2e10d102d
shin_yehee <zia7894@naver.com>
2024-03-21 10:26:16 +0900

    장바구니 추가 기능 중, 좀 더 단순하게 수정

--- src/main/java/com/shopping/hee/service/CartService.java
+++ src/main/java/com/shopping/hee/service/CartService.java
@@ -4,11 +4,7 @@ import com.shopping.hee.domain.Cart;
 import com.shopping.hee.domain.Item;
 import com.shopping.hee.domain.Member;
 import com.shopping.hee.repository.CartRepository;
-import com.shopping.hee.repository.ItemRepository;
-import com.shopping.hee.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,16 +13,12 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class CartService {
     private final CartRepository cartRepository;
-    private final MemberRepository memberRepository;
+    private final MemberService memberService;
     private final ItemService itemService;
 
     public Cart addCart(Long itemId, int count) {
-        // 현재 사용자의 정보
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        // 사용자 아이디를 가져옴
-        String userId = authentication.getName();
 
-        Member member = memberRepository.findByEmail(userId);
+        Member member = memberService.nowMember();
         Item item = itemService.findById(itemId);
 
         validateDuplicateCart(member, item);
