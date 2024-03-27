e11a4107b3b2a97597e7a8babd926e6d60179bcc
shin_yehee <zia7894@naver.com>
2024-03-27 10:30:46 +0900

    회원별 기본 배송지 여부 확인하는 메서드 추가

--- src/main/java/com/shopping/hee/repository/MemberAddressRepository.java
+++ src/main/java/com/shopping/hee/repository/MemberAddressRepository.java
@@ -1,5 +1,6 @@
 package com.shopping.hee.repository;
 
+import com.shopping.hee.domain.Enum.YesNo;
 import com.shopping.hee.domain.Member;
 import com.shopping.hee.domain.MemberAddress;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,4 +13,7 @@ public interface MemberAddressRepository extends JpaRepository<MemberAddress,Lon
 
     // 회원 별 배송지 목록 가져오기
     List<MemberAddress> findByMember(Member member);
+
+    // 회원별 기본 배송지가 있는지 확인
+    MemberAddress findByMemberAndBasic(Member member, YesNo basic);
 }
